29086d340319b37afac625b2600c5a6da5ff440b
dlachaumepalo <dlachaumepalo@users.noreply.github.com>
2024-03-08 10:12:10 +0000

    deploy: 5024ba44f7985ff1f24e64786a523483580448c6

---
+++ doc/assets/js/d07df43d.e37af71d.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmithril_doc=self.webpackChunkmithril_doc||[]).push([[2512],{38082:e=>{e.exports=JSON.parse('[{"label":"Accepted","permalink":"/doc/adr/tags/accepted","count":6},{"label":"Superseded","permalink":"/doc/adr/tags/superseded","count":1}]')}}]);
